a697ac62b4c59479e46e1cdea269fd044004edef
stpoa <stpoa@icloud.com>
2017-11-20 11:23:59 +0100

    Initial commit

---
+++ contracts/ConvertLib.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.4;
+
+library ConvertLib {
+	function convert(uint amount, uint conversionRate) public returns (uint convertedAmount) {
+		return amount * conversionRate;
+	}
+}
